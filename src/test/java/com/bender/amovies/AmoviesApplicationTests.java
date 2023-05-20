5f3a8b90bba1013e229db95d846da5e7b44ebfc6
benderang26 <benderang26@gmail.com>
2023-05-20 16:46:57 +0800

    initial commit

---
+++ src/test/java/com/bender/amovies/AmoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bender.amovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
