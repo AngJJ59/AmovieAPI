6877774bddbd86b625ca5c89b6834fc9f0fe3785
benderang26 <benderang26@gmail.com>
2023-05-21 23:08:19 +0800

    Create POST REQUEST for review by movie imdbId

---
+++ src/main/java/com/bender/amovies/controllers/ReviewController.java
@@ -0,0 +1,26 @@
+package com.bender.amovies.controllers;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bender.amovies.model.Review;
+import com.bender.amovies.services.ReviewService;
+
+@RestController
+@RequestMapping("/api/v1/reviews")
+public class ReviewController {
+    @Autowired
+    private ReviewService reviewService;
+
+    @PostMapping
+    public ResponseEntity<Review> createReview(@RequestBody Map<String, String> payload) {
+        return new ResponseEntity<Review>(reviewService.createReview(payload.get("imdbId"),payload.get("reviewBody")), HttpStatus.CREATED);
+    }
+}
